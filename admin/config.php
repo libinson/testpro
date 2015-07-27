5f63ba0a37346a0ed2c96765f81bf26514217338
Libin <libinson@qusol.org>
2015-07-27 15:35:17 +0530

    Intialising

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/lab/CI_work/admin/');
+define('HTTP_CATALOG', 'http://localhost/lab/CI_work/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/lab/CI_work/admin/');
+define('HTTPS_CATALOG', 'http://localhost/lab/CI_work/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/lab/CI_work/admin/');
+define('DIR_SYSTEM', '/var/www/html/lab/CI_work/system/');
+define('DIR_LANGUAGE', '/var/www/html/lab/CI_work/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/lab/CI_work/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/lab/CI_work/system/config/');
+define('DIR_IMAGE', '/var/www/html/lab/CI_work/image/');
+define('DIR_CACHE', '/var/www/html/lab/CI_work/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/lab/CI_work/system/download/');
+define('DIR_UPLOAD', '/var/www/html/lab/CI_work/system/upload/');
+define('DIR_LOGS', '/var/www/html/lab/CI_work/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/lab/CI_work/system/modification/');
+define('DIR_CATALOG', '/var/www/html/lab/CI_work/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'pass');
+define('DB_DATABASE', 'Ci_work');
+define('DB_PREFIX', 'oc_');
